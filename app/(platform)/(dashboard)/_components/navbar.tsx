eb72d59bc3d3b31100acc686327f72a43b125f86
hang nhung <hangbichnhung@gmail.com>
2024-08-08 17:21:06 +0700

    feature - set up organization

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,46 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size={"sm"}
+          className="rounded-sm hidden md:block h-auto px-2 py-1.5">
+          Create
+        </Button>
+        <Button size={"sm"} className="rounded block md:hidden">
+          <Plus className="w-5 h-5" />
+        </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl={"/organization/:id"}
+          afterLeaveOrganizationUrl="select-org"
+          afterSelectOrganizationUrl={"/organization/:id"}
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          appearance={{ elements: { avatarBox: { height: 32, width: 32 } } }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,31 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+
+export default clerkMiddleware((auth, request) => {
+  const { userId, orgId, redirectToSignIn } = auth();
+  if (userId && isPublicRoute(request)) {
+    let path = "/select-org";
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+
+    const orgSelection = new URL(path, request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  // Handle users who aren't authenticated
+  if (!userId && !isPublicRoute(request)) {
+    return redirectToSignIn({ returnBackUrl: request.url });
+  }
+
+  // Redirect users who are authenticated but not in an org to the org selection page
+  if (userId && !orgId && request.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+});
 
 export const config = {
   matcher: [
