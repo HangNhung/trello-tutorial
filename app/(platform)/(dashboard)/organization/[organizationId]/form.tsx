fdd83572751a4ac3e5f79ea9cce311faf0c7f448
hang nhung <hangbichnhung@gmail.com>
2024-08-10 10:53:15 +0700

    feature - useAction abstraction

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,19 +1,28 @@
 "use client";
-
 import { createBoard } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
-import { useFormState } from "react-dom";
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
+import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
-  const initialState = { message: "", errors: {} };
-  const [state, dispatch] = useFormState(createBoard, initialState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log("success", data);
+    },
+    onError: (error) => {
+      console.log("error", error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title });
+  };
 
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className="flex flex-col gap-y-2">
-        <FormInput errors={state?.errors} />
+        <FormInput errors={fieldErrors} />
         <FormButton variant="primary" text="Submit" type="submit" />
       </div>
     </form>
