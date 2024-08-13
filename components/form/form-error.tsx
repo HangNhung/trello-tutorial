e2e7315aad34cafafa588514f7b75cdbad1d9957
hang nhung <hangbichnhung@gmail.com>
2024-08-13 14:42:06 +0700

    feature - build form popover

--- components/form/form-error.tsx
+++ components/form/form-error.tsx
@@ -1,19 +1,19 @@
 import { XCircle } from "lucide-react";
 
-interface FormErrorProps {
+interface FormErrorsProps {
   errors?: Record<string, string[] | undefined>;
   id: string;
 }
 
-export const FormErrors = ({ errors, id }: FormErrorProps) => {
-  if (!errors || !errors[id]) return null;
+export const FormErrors = ({ errors, id }: FormErrorsProps) => {
+  if (!errors) return null;
 
   return (
     <div
       id={`${id}-error`}
       aria-live="polite"
       className="mt-2 text-xs text-rose-500">
-      {errors[id]?.map((error) => (
+      {errors[id]?.map((error: string) => (
         <div
           key={error}
           className="flex items-center font-medium p-2 border border-rose-500 bg-rose-500/10">
